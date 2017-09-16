320bbd7721f9db107c88b17f43fa94e7af8618d6
kumudug <kumudug1@gmail.com>
2017-09-16 18:18:03 +1000

    Services

--- app/app.module.ts
+++ app/app.module.ts
@@ -5,10 +5,12 @@ import { EventsAppComponent } from './events-app.component'
 import { EventsListComponent } from './events/events-list.component'
 import { EventThumbnailComponent } from './events/event-thumbnail.component'
 import { NavBarComponent } from './nav/navbar.component'
+import { EventSerice } from './events/shared/events.service'
 
 @NgModule({
     imports: [BrowserModule],
     declarations: [EventsAppComponent, EventsListComponent, EventThumbnailComponent, NavBarComponent],
+    providers: [EventSerice],
     bootstrap: [EventsAppComponent]
 })
 export class AppModule { }
