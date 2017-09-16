9196d7518e4f4a660469c9a6584bd065bece6096
kumudug <kumudug1@gmail.com>
2017-09-16 18:17:43 +1000

    Angular template syntax

--- app/events/event-thumbnail.component.ts
+++ app/events/event-thumbnail.component.ts
@@ -4,18 +4,29 @@ import { Component, Input, Output, EventEmitter } from '@angular/core'
     selector: 'event-thumbnail',
     template: `
     <div class="well hoverwell thumbnail">
-        <h2>{{event.name}}</h2>
-        <div>Date: {{event.date}}</div>
-        <div>Time: {{event.time}}</div>
-        <div>Price: \${{event.price}}</div>
-        <div>
+        <h2>{{event?.name}}</h2>
+        <div>Date: {{event?.date}}</div>
+        <div [ngSwitch]="event?.time" [ngClass]="{green: event?.time === '8:00 am', bold: (event?.time === '8:00 am' || event?.time === '10:00 am')}">
+            Time: {{event?.time}}
+            <span *ngSwitchCase="'8:00 am'">(Early Start)</span>
+            <span *ngSwitchCase="'10:00 am'">(Late Start)</span>
+            <span *ngSwitchDefault>(Normal Start)</span>
+        </div>
+        <div>Price: \${{event?.price}}</div>
+        <div *ngIf="event?.location">
             <span>Location: {{event.location.address}}</span>
             <span class="pad-left">{{event.location.city}}, {{event.location.contry}}</span>
+        </div>
+        <div [hidden]="!event?.onlineUrl">
+            Online URL: {{event.onlineUrl}}
         </div>
         <!-- <button class="btn btn-primary" (click)="handleClickMe()">Click me!</button> -->
     </div>
     `,
     styles: [`
+        .green { color: #003300 !important; }
+        .bold { font-weight: bold; }
+        .thumbnail { min-height: 210px; }
         .pad-left { margin-left: 10px; }
         .well div { color: #bbb }
     `]
--- app/events/events-list.component.ts
+++ app/events/events-list.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core'
+import { Component, OnInit } from '@angular/core'
+import { EventSerice } from './shared/events.service'
 
 @Component({
     selector: 'events-list',
@@ -7,24 +8,24 @@ import { Component } from '@angular/core'
         <h1>Upcoming Angular 2 Events</h1>
         <hr/>
         <!--<event-thumbnail [event]="event1" (eventClick)="handleEventClicked($event)"></event-thumbnail>-->
-        <event-thumbnail #thumbnail [event]="event1"></event-thumbnail>
+        <div class="row">
+            <div *ngFor="let event of events" class="col-md-5">
+                <event-thumbnail #thumbnail [event]="event"></event-thumbnail>
+            </div>
+        </div>
+        <!--<event-thumbnail #thumbnail *ngFor="let event of events" [event]="event"></event-thumbnail>-->
         <!--<button class="btn btn-primary" (click)="thumbnail.logFoo()">Log me some foo</button>-->
     </div>
     `
 })
-export class EventsListComponent{
-    event1 = {
-        id: 1,
-        name: 'Angular Connect',
-        date: '9/26/2036',
-        time: '10:00 am',
-        price: 599.99,
-        imageUrl: '/app/assets/images/angularconnect-shield.png',
-        location: {
-            address: '1057 DT',
-            city: 'London',
-            contry: 'England'
-        }
+export class EventsListComponent implements OnInit {
+    events: any[]
+    constructor(private eventService: EventSerice) {
+        this.events = this.eventService.getEvents();
+    }
+
+    ngOnInit() {
+        this.events = this.eventService.getEvents()
     }
 
     // handleEventClicked(data){
